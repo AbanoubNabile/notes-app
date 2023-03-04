d65caf5c446c184230b244a7cdb853c1f58746a5
Abanoub Nabil <bano77nabil77@gmail.com>
2023-03-04 18:35:41 +0200

    create file notes views body

---
+++ lib/views/widgets/notes_views_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesVieswBody extends StatelessWidget {
+  const NotesVieswBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
