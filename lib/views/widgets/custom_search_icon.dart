f475e9ef63a192647b63949404583c93f226952b
Abanoub Nabil <bano77nabil77@gmail.com>
2023-03-04 20:15:17 +0200

    change edge body

--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -5,19 +5,16 @@ class CustomSearchIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 45,
-        width: 45,
-        decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.0),
-            borderRadius: BorderRadius.circular(16)),
-        child: const Center(
-          child: Icon(
-            Icons.search,
-            size: 30,
-          ),
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.0),
+          borderRadius: BorderRadius.circular(16)),
+      child: const Center(
+        child: Icon(
+          Icons.search,
+          size: 30,
         ),
       ),
     );
