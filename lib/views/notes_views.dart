6272efd97d5fd769364d244c118e003106f9ba56
Abanoub Nabil <bano77nabil77@gmail.com>
2023-03-04 18:40:42 +0200

    create app bar

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_views_body.dart';
 
 class NotesViews extends StatelessWidget {
   const NotesViews({super.key});
@@ -8,14 +9,5 @@ class NotesViews extends StatelessWidget {
     return Scaffold(
       body: NotesVieswBody(),
     );
-  }
-}
-
-class NotesVieswBody extends StatelessWidget {
-  const NotesVieswBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
