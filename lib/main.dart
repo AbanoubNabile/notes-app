cb73131972a7d709faea9f3f7cdfe22bf30da2a7
Abanoub Nabil <bano77nabil77@gmail.com>
2023-03-04 17:59:28 +0200

    create widget notes_app

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const NotesApp());
 }
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
