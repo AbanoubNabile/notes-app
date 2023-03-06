2ed2d696b4311c8668b8e7cabdbaede21863cab5
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-06 20:58:02 +0200

    create file note item app

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: const TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 24, bottom: 24),
+                child: const Text(
+                  'Build your career with Abanoub Nabil',
+                  style: TextStyle(
+                      color: Color.fromARGB(131, 0, 0, 0), fontSize: 20),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 32,
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text('March 6  2023',
+                style: TextStyle(
+                    color: Color.fromARGB(131, 0, 0, 0), fontSize: 16)),
+          )
+        ],
+      ),
+    );
+  }
+}
