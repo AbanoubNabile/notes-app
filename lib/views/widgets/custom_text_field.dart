25e491b1b4711c8e7a8da8aa7846c6fd7ab4d12d
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-08 01:17:33 +0200

    add string hint and max line to text field

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint, required this.maxLine});
+  final String hint;
+  final int maxLine;
   @override
   Widget build(BuildContext context) {
     return TextField(
