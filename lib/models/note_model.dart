6f50be1364bd4bac7836969003a799a2415ebad2
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-08 17:45:14 +0200

    create new class note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,1 +1,12 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String Date;
+  final int color;
+
+  NoteModel(this.title, this.subTitle, this.Date, this.color);
+}
