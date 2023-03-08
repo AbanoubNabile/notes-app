8bb1c6e98948ea814e38e1d3538da8f30960d4db
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-08 21:47:56 +0200

    create add note cubit

---
+++ lib/cubits/add_note_cubit/notes_cubit.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+import "package:bloc/bloc.dart";
+import 'package:meta/meta.dart';
+
+part 'note_state.dart';
+
+class NoteCubit extends Cubit<NoteState> {
+  NoteCubit() : super(NotesInitial());
+}
